4e66ef09bc930110ebbdeafa05da81df88d15e3f
Antonis Kalou <kalouantonis@protonmail.com>
2021-04-22 17:14:30 +0200

    Initial commit
    Project boilerplate

---
+++ lib/bonfire_reflow.ex
@@ -0,0 +1,2 @@
+defmodule Bonfire.Reflow do
+end
---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule BonfireFiles.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bonfire_reflow,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: Mess.deps []
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
